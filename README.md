fd5ccafa491639241703595f79ae390da9c62d18
isura <isura@wso2.com>
2022-09-04 04:37:53 +0530

    Reformat the code

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # drone
 
+Swagger definition : https://app.swaggerhub.com/apis/ISURADILHARA/swagger-drone_open_api_3_0/1.0.0
 
-Assumptions
-     - Load the medicines to drone in one time. If it is required to load 
-additional medicine, need to clear the drawn first and load all together.
-     - Assume the image is stored as the image URL. (not as the binary) 
+Prerequisites 
+     - Apache Maven 3.8.5
+     - Apache-tomcat-9.0.65
+     - Java 11
+How to build
+     - Clone the project from https://github.com/IsuraD/drone
+       (git clone https://github.com/IsuraD/drone)
+     - Go to the root folder and execute mvn clean install
+How to Run
+     - copy the drone.war file to <TOMCAT_HOME>/webapps/ directory
+     - Start the tomcat 'sh bin/startup.sh'
+
+
+
--- src/main/java/io/drone/api/dao/DroneDAO.java
+++ src/main/java/io/drone/api/dao/DroneDAO.java
@@ -12,10 +12,14 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class is used to communicate with the Database layer.
+ */
 public class DroneDAO {
     private static final DroneDAO droneDAO = new DroneDAO();
 
     private DroneDAO() {
+        // Initialize the initial databases since this is a in-memory database.
         initializeDatabase();
     }
 
@@ -23,7 +27,11 @@ public class DroneDAO {
         return droneDAO;
     }
 
-
+    /**
+     * Add a drone
+     *
+     * @param body
+     */
     public void addDrone(Drone body) {
         try {
             Connection connection = getConnection();
@@ -40,6 +48,11 @@ public class DroneDAO {
         }
     }
 
+    /**
+     * Add a medicine
+     *
+     * @param medicine
+     */
     public void addMedicine(Medicine medicine) {
         try {
             validateInput(medicine);
@@ -54,6 +67,173 @@ public class DroneDAO {
         } catch (SQLException e) {
             System.out.println("Error while adding medicine:" + medicine.getName() + ": " + e);
         }
+    }
+
+
+    /**
+     * Get a Drone
+     *
+     * @param serialNumber
+     * @return
+     */
+    public DroneGet getDrone(String serialNumber) {
+        try {
+            Connection connection = getConnection();
+            PreparedStatement prepStmt = connection.prepareStatement(SQLConstants.LOAD_DRONE);
+            prepStmt.setString(1, serialNumber);
+
+            try (ResultSet rs = prepStmt.executeQuery()) {
+                if (rs.next()) {
+                    DroneGet droneGet = new DroneGet();
+                    droneGet.setSeriealNumber(serialNumber);
+                    droneGet.setWeightLimit(rs.getInt("WEIGHT_LIMIT"));
+                    droneGet.setState(DroneGet.StateEnum.fromValue(rs.getString("STATE")));
+                    droneGet.setBatteryCapacity(rs.getInt("BATTERY_CAPACITY"));
+                    droneGet.setModel(DroneGet.ModelEnum.fromValue(rs.getString("MODEL")));
+                    LoadDrone loadDrone = new LoadDrone();
+                    loadDrone.setMedicine(getMedicines(serialNumber));
+                    droneGet.setLoad(loadDrone);
+                    return droneGet;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while lading the drone:" + serialNumber + ": " + e);
+        }
+        return null;
+    }
+
+    /**
+     * Get a medicine by name
+     *
+     * @param name
+     * @return
+     */
+    public Medicine getMedicine(String name) {
+        try {
+            Connection connection = getConnection();
+            PreparedStatement prepStmt = connection.prepareStatement(SQLConstants.LOAD_MEDICINE);
+            prepStmt.setString(1, name);
+
+            try (ResultSet rs = prepStmt.executeQuery()) {
+                if (rs.next()) {
+                    Medicine medicine = new Medicine();
+                    medicine.setCode(rs.getString("CODE"));
+                    medicine.setImageURL(rs.getString("IMAGE_URL"));
+                    medicine.setName(name);
+                    medicine.setWeight(rs.getInt("WEIGHT"));
+                    return medicine;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while lading the medicine:" + name + ": " + e);
+        }
+        return null;
+    }
+
+    /**
+     * Load the draone with medicine.
+     *
+     * @param body
+     * @param serialNumber
+     * @return
+     */
+    public LoadDrone loadDrone(LoadDrone body, String serialNumber) {
+        //Validate the load.
+        validateLoad(body, serialNumber);
+        Map<String, Integer> medicines = getMedicineMap(body.getMedicine());
+        Map<String, Medicine> nameToMedicineMap = getNameToMedicineMap(body.getMedicine());
+        try {
+            Connection connection = getConnection();
+
+            for (String medicineName : medicines.keySet()) {
+                if (getMedicine(medicineName) == null) {
+                    addMedicine(nameToMedicineMap.get(medicineName));
+                }
+                int count = medicines.get(medicineName);
+                PreparedStatement prepStmt = connection.prepareStatement(SQLConstants.ADD_DRONE_MEDICINE);
+                prepStmt.setString(1, serialNumber);
+                prepStmt.setString(2, medicineName);
+                prepStmt.setInt(3, count);
+                prepStmt.execute();
+
+                PreparedStatement prepStmt2 = connection.prepareStatement(SQLConstants.UPDATE_DRONE_STATE);
+                prepStmt2.setString(1, Drone.StateEnum.LOADED.toString());
+                prepStmt2.setString(2, serialNumber);
+                prepStmt2.execute();
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Error while loading medicine to drone:" + serialNumber + ": " + e);
+        }
+        return body;
+    }
+
+    /**
+     * Get the idle drones.
+     * @return
+     */
+    public List<DroneSearch> getAvailableDronesToLoad() {
+        try {
+            Connection connection = getConnection();
+            PreparedStatement prepStmt = connection.prepareStatement(SQLConstants.LOAD_AVAILABLE_DRONES);
+            prepStmt.setString(1, Drone.StateEnum.IDLE.toString());
+            List<DroneSearch> drones = new ArrayList<>();
+            try (ResultSet rs = prepStmt.executeQuery()) {
+                while (rs.next()) {
+                    DroneSearch droneSearch = new DroneSearch();
+                    DroneGet drone = getDrone(rs.getString("SERIAL_NUMBER"));
+                    droneSearch.setCurrentLoad(drone.getLoad());
+                    droneSearch.setBatteryCapacity(drone.getBatteryCapacity());
+                    droneSearch.setState(DroneSearch.StateEnum.IDLE);
+                    droneSearch.setSeriealNumber(drone.getSeriealNumber());
+                    droneSearch.setWeightLimit(drone.getWeightLimit());
+                    droneSearch.setWeightRemaining(drone.getWeightLimit());
+                    drones.add(droneSearch);
+                }
+            }
+            return drones;
+        } catch (SQLException e) {
+            System.out.println("Error while lading the available drones" + e);
+        }
+        return null;
+    }
+
+    private void validateLoad(LoadDrone body, String serialNumber) {
+        DroneGet drone = getDrone(serialNumber);
+        int maxWeight = drone.getWeightLimit();
+        Integer batteryCapacity = drone.getBatteryCapacity();
+
+        int totalWeight = 0;
+        for (Medicine medicine : body.getMedicine()) {
+            totalWeight = totalWeight + medicine.getWeight();
+        }
+        if (totalWeight > maxWeight) {
+            throw new BadRequestException("Max Allowed weight exceeded.");
+        }
+
+        if (batteryCapacity < 25) {
+            throw new BadRequestException("Battery capacity is not enough to load the drone.");
+        }
+    }
+
+    private Map<String, Integer> getMedicineMap(List<Medicine> medicines) {
+        Map<String, Integer> map = new HashMap<>();
+        for (Medicine medicine : medicines) {
+            if (map.get(medicine.getName()) == null) {
+                map.put(medicine.getName(), 1);
+            } else {
+                map.put(medicine.getName(), map.get(medicine.getName() + 1));
+            }
+        }
+        return map;
+    }
+
+    private Map<String, Medicine> getNameToMedicineMap(List<Medicine> medicines) {
+        Map<String, Medicine> map = new HashMap<>();
+        for (Medicine medicine : medicines) {
+            map.putIfAbsent(medicine.getName(), medicine);
+        }
+        return map;
     }
 
     private void validateInput(Medicine medicine) {
@@ -92,54 +272,6 @@ public class DroneDAO {
         return DriverManager.getConnection(jdbcURL);
     }
 
-    public DroneGet getDrone(String serialNumber) {
-        try {
-            Connection connection = getConnection();
-            PreparedStatement prepStmt = connection.prepareStatement(SQLConstants.LOAD_DRONE);
-            prepStmt.setString(1, serialNumber);
-
-            try (ResultSet rs = prepStmt.executeQuery()) {
-                if (rs.next()) {
-                    DroneGet droneGet = new DroneGet();
-                    droneGet.setSeriealNumber(serialNumber);
-                    droneGet.setWeightLimit(rs.getInt("WEIGHT_LIMIT"));
-                    droneGet.setState(DroneGet.StateEnum.fromValue(rs.getString("STATE")));
-                    droneGet.setBatteryCapacity(rs.getInt("BATTERY_CAPACITY"));
-                    droneGet.setModel(DroneGet.ModelEnum.fromValue(rs.getString("MODEL")));
-                    LoadDrone loadDrone = new LoadDrone();
-                    loadDrone.setMedicine(getMedicines(serialNumber));
-                    droneGet.setLoad(loadDrone);
-                    return droneGet;
-                }
-            }
-        } catch (SQLException e) {
-            System.out.println("Error while lading the drone:" + serialNumber + ": " + e);
-        }
-        return null;
-    }
-
-    public Medicine getMedicine(String name) {
-        try {
-            Connection connection = getConnection();
-            PreparedStatement prepStmt = connection.prepareStatement(SQLConstants.LOAD_MEDICINE);
-            prepStmt.setString(1, name);
-
-            try (ResultSet rs = prepStmt.executeQuery()) {
-                if (rs.next()) {
-                    Medicine medicine = new Medicine();
-                    medicine.setCode(rs.getString("CODE"));
-                    medicine.setImageURL(rs.getString("IMAGE_URL"));
-                    medicine.setName(name);
-                    medicine.setWeight(rs.getInt("WEIGHT"));
-                    return medicine;
-                }
-            }
-        } catch (SQLException e) {
-            System.out.println("Error while lading the medicine:" + name + ": " + e);
-        }
-        return null;
-    }
-
     private Map<String, Integer> getDroneMedicine(String serialNumber) {
         try {
             Connection connection = getConnection();
@@ -172,90 +304,4 @@ public class DroneDAO {
         return medicines;
     }
 
-    public LoadDrone loadDrone(LoadDrone body, String serialNumber) {
-        validateLoad(body, serialNumber);
-        Map<String, Integer> medicines = getMedicineMap(body.getMedicine());
-        Map<String, Medicine> nameToMedicineMap = getNameToMedicineMap(body.getMedicine());
-        try {
-            Connection connection = getConnection();
-
-            for (String medicineName : medicines.keySet()) {
-                if (getMedicine(medicineName) == null) {
-                    addMedicine(nameToMedicineMap.get(medicineName));
-                }
-                int count = medicines.get(medicineName);
-                PreparedStatement prepStmt = connection.prepareStatement(SQLConstants.ADD_DRONE_MEDICINE);
-                prepStmt.setString(1, serialNumber);
-                prepStmt.setString(2, medicineName);
-                prepStmt.setInt(3, count);
-                prepStmt.execute();
-
-                PreparedStatement prepStmt2 = connection.prepareStatement(SQLConstants.UPDATE_DRONE_STATE);
-                prepStmt2.setString(1, Drone.StateEnum.LOADED.toString());
-                prepStmt2.setString(2, serialNumber);
-                prepStmt2.execute();
-            }
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Error while loading medicine to drone:" + serialNumber + ": " + e);
-        }
-        return body;
-    }
-
-    public List<DroneSearch> getAvailableDronesToLoad() {
-        try {
-            Connection connection = getConnection();
-            PreparedStatement prepStmt = connection.prepareStatement(SQLConstants.LOAD_AVAILABLE_DRONES);
-            prepStmt.setString(1, Drone.StateEnum.IDLE.toString());
-            List<DroneSearch> drones = new ArrayList<>();
-            try (ResultSet rs = prepStmt.executeQuery()) {
-                while (rs.next()) {
-                    DroneSearch droneSearch = new DroneSearch();
-                    DroneGet drone = getDrone(rs.getString("SERIAL_NUMBER"));
-                    droneSearch.setCurrentLoad(drone.getLoad());
-                    droneSearch.setBatteryCapacity(drone.getBatteryCapacity());
-                    droneSearch.setState(DroneSearch.StateEnum.IDLE);
-                    droneSearch.setSeriealNumber(drone.getSeriealNumber());
-                    droneSearch.setWeightLimit(drone.getWeightLimit());
-                    droneSearch.setWeightRemaining(drone.getWeightLimit());
-                    drones.add(droneSearch);
-                }
-            }
-            return drones;
-        } catch (SQLException e) {
-            System.out.println("Error while lading the available drones" + e);
-        }
-        return null;
-    }
-
-    private void validateLoad(LoadDrone body, String serialNumber) {
-        int maxWeight = getDrone(serialNumber).getWeightLimit();
-        int totalWeight = 0;
-        for (Medicine medicine : body.getMedicine()) {
-            totalWeight = totalWeight + medicine.getWeight();
-        }
-        if (totalWeight > maxWeight) {
-            throw new BadRequestException("Max Allowed weight exceeded.");
-        }
-    }
-
-    private Map<String, Integer> getMedicineMap(List<Medicine> medicines) {
-        Map<String, Integer> map = new HashMap<>();
-        for (Medicine medicine : medicines) {
-            if (map.get(medicine.getName()) == null) {
-                map.put(medicine.getName(), 1);
-            } else {
-                map.put(medicine.getName(), map.get(medicine.getName() + 1));
-            }
-        }
-        return map;
-    }
-
-    private Map<String, Medicine> getNameToMedicineMap(List<Medicine> medicines) {
-        Map<String, Medicine> map = new HashMap<>();
-        for (Medicine medicine : medicines) {
-            map.putIfAbsent(medicine.getName(), medicine);
-        }
-        return map;
-    }
 }
